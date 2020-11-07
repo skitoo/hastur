e1baf9bc29952c9c5f4201a514fef0c3880f48a5
Alexis Couronne <alexis@skitoo.net>
2020-11-07 18:11:34 +0100

    feat(shared_kernel): add Aggrate abstract entity

---
+++ hastur/domain/shared_kernel/entity.py
@@ -0,0 +1,72 @@
+import logging
+from abc import ABC
+from uuid import UUID
+from .event import EventStream, DomainEvent
+
+logger = logging.getLogger(__name__)
+
+
+class AggregateError(Exception):
+    pass
+
+
+class HandlerNotFoundError(AggregateError):
+    def __init__(self, handler: str, aggregate: "Aggregate"):
+        super().__init__(
+            f"Handle '{handler}' not found on '{aggregate.__class__.__name__}' aggregate"
+        )
+
+
+class EventVersionError(AggregateError):
+    def __init__(self, event: DomainEvent, aggregate: "Aggregate"):
+        super().__init__(
+            f"Event version '{event.version}' does not match \
+            expected version '{aggregate.next_version}' of \
+            aggregate '{aggregate.__class__.__name__}'"
+        )
+
+
+class Aggregate(ABC):
+    def __init__(self, id_: UUID, stream: EventStream = None):
+        self.__id: UUID = id_
+        self.__stream: EventStream = stream or []
+        self.__version: int = 0
+        self.__new_events: EventStream = []
+        self.__replay_events()
+
+    def __replay_events(self):
+        for event in self.__stream:
+            self.__apply(event)
+            self.__version = event.version
+
+    def __apply(self, event: DomainEvent):
+        handler = event.handler_name
+        if hasattr(self, handler) and callable(getattr(self, handler)):
+            getattr(self, handler)(event)
+        else:
+            raise HandlerNotFoundError(handler, self)
+
+    def apply_new_event(self, event: DomainEvent):
+        if event.version != self.next_version:
+            raise EventVersionError(event, self)
+        self.__apply(event)
+        self.__new_events.append(event)
+
+    def get_id(self):
+        return self.__id
+
+    @property
+    def new_events(self) -> EventStream:
+        return self.__new_events.copy()
+
+    @property
+    def stream(self) -> EventStream:
+        return self.__stream.copy()
+
+    @property
+    def next_version(self) -> int:
+        return self.__version + len(self.__new_events) + 1
+
+    @property
+    def base_version(self) -> int:
+        return self.__version
---
+++ hastur/domain/shared_kernel/test_entity.py
@@ -0,0 +1,104 @@
+from unittest import TestCase
+from unittest.mock import Mock, call
+from uuid import uuid4, UUID
+from datetime import datetime
+import pytest
+from .entity import Aggregate, EventVersionError, HandlerNotFoundError
+from .event import DomainEvent, EventStream
+
+
+class DownloadCreatedEvent(DomainEvent):
+    pass
+
+
+class UnknownEvent(DomainEvent):
+    pass
+
+
+class MyEntity(Aggregate):
+    def __init__(self, id_: UUID, mock: Mock, stream: EventStream):
+        self.mock = mock
+        super().__init__(id_, stream)
+
+    def on_download_created(self, event: DownloadCreatedEvent):
+        self.mock.event_called(event)
+
+
+class TestAggregate(TestCase):
+    def setUp(self):
+        self.id_ = uuid4()
+        self.stream = []
+        self.now = datetime.now()
+        self.version = 1
+        self.mock = Mock()
+        self.instance = MyEntity(self.id_, self.mock, self.stream)
+
+    def test_get_id(self):
+        self.assertEqual(self.instance.get_id(), self.id_)
+
+    def test_stream(self):
+        self.assertEqual(self.instance.stream, self.stream)
+        self.assertNotEqual(id(self.instance.stream), id(self.stream))
+
+    def test_base_version(self):
+        self.assertEqual(self.instance.base_version, 0)
+
+    def test_apply_new_event_with_success(self):
+        event = DownloadCreatedEvent(self.id_, self.now, self.version)
+        self.instance.apply_new_event(event)
+
+        self.mock.event_called.assert_called_once_with(event)
+        self.assertEqual(self.instance.new_events, [event])
+        self.assertEqual(self.instance.stream, [])
+
+    def test_apply_new_event_with_invalid_version(self):
+        event = DownloadCreatedEvent(self.id_, self.now, 100)
+        with pytest.raises(EventVersionError):
+            self.instance.apply_new_event(event)
+
+        self.mock.event_called.assert_not_called()
+        self.assertEqual(self.instance.new_events, [])
+
+    def test_apply_new_event_with_unknown_event(self):
+        event = UnknownEvent(self.id_, self.now, self.version)
+        with pytest.raises(HandlerNotFoundError):
+            self.instance.apply_new_event(event)
+
+        self.mock.event_called.assert_not_called()
+        self.assertEqual(self.instance.new_events, [])
+
+    def test_next_version(self):
+        event = DownloadCreatedEvent(self.id_, self.now, self.version)
+
+        self.assertEqual(self.instance.next_version, 1)
+        self.instance.apply_new_event(event)
+        self.assertEqual(self.instance.next_version, 2)
+
+    def test___replay_events_with_success(self):
+        event1 = DownloadCreatedEvent(self.id_, self.now, 1)
+        event2 = DownloadCreatedEvent(self.id_, self.now, 2)
+        event3 = DownloadCreatedEvent(self.id_, self.now, 3)
+        stream = [event1, event2, event3]
+        instance = MyEntity(self.id_, self.mock, stream)
+
+        self.mock.event_called.assert_has_calls(
+            [
+                call(event1),
+                call(event2),
+                call(event3),
+            ],
+        )
+        self.assertEqual(self.mock.event_called.call_count, 3)
+        self.assertEqual(instance.new_events, [])
+        self.assertEqual(instance.stream, stream)
+        self.assertNotEqual(id(instance.stream), id(stream))
+        self.assertEqual(instance.next_version, 4)
+        self.assertEqual(instance.base_version, 3)
+
+    def test___replay_events_with_fail(self):
+        event1 = DownloadCreatedEvent(self.id_, self.now, 1)
+        event2 = UnknownEvent(self.id_, self.now, 2)
+        stream = [event1, event2]
+
+        with pytest.raises(HandlerNotFoundError):
+            MyEntity(self.id_, self.mock, stream)
