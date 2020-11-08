251149e3df905991a738d7b95f97c328810315ba
Alexis Couronne <alexis@skitoo.net>
2020-11-08 12:06:24 +0100

    feat(shared_kernel): add payload parameter in DomainEvent

--- hastur/domain/shared_kernel/event.py
+++ hastur/domain/shared_kernel/event.py
@@ -1,14 +1,33 @@
 from uuid import UUID
+from dataclasses import dataclass, is_dataclass
 from datetime import datetime
 from typing import List
 import re
 
 
+class EventError(Exception):
+    def __init__(self, event: "DomainEvent"):
+        name = event.__class__.__name__
+        type_ = type(event.payload)
+        super().__init__(
+            f"{name} waiting its own Payload (a <class: dataclass>) as parameter. Received: {type_}"
+        )
+
+
 class DomainEvent:
-    def __init__(self, id_: UUID, created_at: datetime, version: int):
+    @dataclass
+    class Payload:
+        pass
+
+    def __init__(self, id_: UUID, created_at: datetime, version: int, payload=None):
         self.id_: UUID = id_
         self.created_at: datetime = created_at
         self.version: int = version
+        self.payload = payload
+        if payload is not None and (
+            not is_dataclass(payload) or not isinstance(payload, self.__class__.Payload)
+        ):
+            raise EventError(self)
 
     def __str__(self):
         name = self.__class__.__name__
--- hastur/domain/shared_kernel/test_event.py
+++ hastur/domain/shared_kernel/test_event.py
@@ -1,6 +1,8 @@
 from uuid import uuid4
+from dataclasses import dataclass
 from datetime import datetime
-from .event import DomainEvent
+import pytest
+from .event import DomainEvent, EventError
 
 
 id_ = uuid4()
@@ -9,11 +11,15 @@ VERSION = 1
 
 
 class DownloadCreatedEvent(DomainEvent):
-    pass
+    @dataclass
+    class Payload:
+        url: str
 
 
 class CardSended(DomainEvent):
-    pass
+    @dataclass
+    class Payload:
+        name: str
 
 
 def test_to_string():
@@ -39,3 +45,17 @@ def test_handler_name():
 
     event = CardSended(id_, now, VERSION)
     assert event.handler_name == "on_card_sended"
+
+
+def test_payload():
+    event = DownloadCreatedEvent(
+        id_, now, VERSION, DownloadCreatedEvent.Payload("toto.com")
+    )
+
+    assert event.payload == DownloadCreatedEvent.Payload("toto.com")
+
+    with pytest.raises(EventError):
+        DownloadCreatedEvent(id_, now, VERSION, "toto.com")
+
+    with pytest.raises(EventError):
+        DownloadCreatedEvent(id_, now, VERSION, CardSended.Payload("Test"))
