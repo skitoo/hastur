5c07e4ad836544e1decd9d9280a68e74afa928a2
Alexis Couronne <alexis@skitoo.net>
2020-11-10 08:55:35 +0100

    feat(download): add DownloadProjection abstract class

---
+++ hastur/domain/download/test_projection.py
@@ -0,0 +1,34 @@
+from uuid import uuid4
+from datetime import datetime
+from hastur.port.event import LocalEventBus
+from .entity.download import DownloadCreatedEvent, DownloadStatus
+from .projection import DownloadProjection, Download
+
+
+class LocalProjection(DownloadProjection):
+    download: Download
+
+    def add(self, download: Download):
+        self.download = download
+
+    def list(self):
+        return
+
+
+def test_download_projection_on_created_event():
+    bus = LocalEventBus()
+    event = DownloadCreatedEvent(
+        uuid4(),
+        datetime.now(),
+        1,
+        DownloadCreatedEvent.Payload(url="http://foo.com", status=DownloadStatus.NEW),
+    )
+    projection = LocalProjection(bus)
+    bus.dispatch([event])
+
+    assert projection.download == Download(
+        id_=event.id_,
+        created_at=event.created_at,
+        status=DownloadStatus.NEW,
+        url="http://foo.com",
+    )
