26a0b708128ab046561c5d631f681130bb7cc2eb
Alexis Couronne <alexis@skitoo.net>
2020-11-11 20:49:58 +0100

    feat(download): add Downloader abstract class

---
+++ hastur/domain/download/task.py
@@ -0,0 +1,17 @@
+from abc import ABC, abstractmethod
+from logging import getLogger, Logger
+from hastur.domain.shared_kernel.event import EventBus
+from hastur.domain.shared_kernel.message import CommandBus
+from hastur.domain.download.entity.download import DownloadCreatedEvent
+
+
+class Downloader(ABC):
+    def __init__(self, event_bus: EventBus, command_bus: CommandBus):
+        self.event_bus: EventBus = event_bus
+        self.command_bus: CommandBus = command_bus
+        self.logger: Logger = getLogger(str(type(self)))
+        self.event_bus.add_handler(DownloadCreatedEvent, self.on_download_created)
+
+    @abstractmethod
+    def on_download_created(self, event: DownloadCreatedEvent):
+        pass
