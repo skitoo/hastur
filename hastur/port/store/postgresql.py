729ec91e5b2442f3d62e394a279674414fbbeb04
Alexis Couronne <alexis@skitoo.net>
2020-11-19 08:55:28 +0100

    WIP

--- hastur/port/store/postgresql.py
+++ hastur/port/store/postgresql.py
@@ -12,34 +12,57 @@ class PgEventStore(EventStore):
     def __init__(self, connection: Connection):
         self.connection: Connection = connection
         self.__create_table()
+        #  self.__create_trigger()
 
     def __create_table(self):
         query = """
         CREATE TABLE IF NOT EXISTS event (
             id SERIAL PRIMARY KEY,
-            type VARCHAR(255) NOT NULL,
-            aggregate_id UUID NOT NULL,
-            aggregate_type VARCHAR(255) NOT NULL,
+            type TEXT NOT NULL,
+            stream UUID NOT NULL,
             version INT NOT NULL,
             payload JSON,
             created_at TIMESTAMP NOT NULL,
-            UNIQUE(aggregate_id, aggregate_type, version)
+            UNIQUE(stream, version)
         );
+        """
+        self.connection.execute(query)
+
+    def __create_trigger(self):
+        query = """
+        CREATE TRIGGER IF NOT EXISTS check_version RETURNS trigger AS test
+        DECLARE
+            last_version INTEGER;
+        BEGIN
+            SELECT version INTO last_version
+            FROM event
+            WHERE stream = NEW.stream
+            ORDER BY version DESC
+            LIMIT 1;
+
+            IF last_version != NEW.version + 1 THEN
+                RAISE EXCEPTION '% version is not good', NEW.version;
+            END IF;
+            RETURN NEW;
+        END
+        test LANGUAGE plpgsql;
+
+        CREATE TRIGGER check_version BEFORE INSERT ON event
+            FOR EACH ROW EXECUTE PROCEDURE check_version();
         """
         self.connection.execute(query)
 
     def save(self, aggregates: AggregateCollection) -> NoReturn:
         query = """
-        INSERT INTO event(type, aggregate_id, aggregate_type, version, payload, created_at)
-        VALUES(:type, :aggregate_id, :aggregate_type, :version, :payload, :created_at)
+        INSERT INTO event(type, stream, version, payload, created_at)
+        VALUES(:type, :stream, :version, :payload, :created_at)
         """
         values = []
         for aggregate in aggregates:
             for event in aggregate.new_events:
                 value = {
                     "type": fullname(event),
-                    "aggregate_id": aggregate.get_id(),
-                    "aggregate_type": fullname(aggregate),
+                    "stream": event.id_,
                     "version": event.version,
                     "payload": event.payload.json() if event.payload else None,
                     "created_at": event.created_at,
@@ -49,9 +72,9 @@ class PgEventStore(EventStore):
 
     def load_stream(self, id_: UUID, aggregate_type: Type[Aggregate]) -> EventStream:
         query = """
-        SELECT type, aggregate_id, created_at, version, payload
+        SELECT type, stream, created_at, version, payload
         FROM event
-        WHERE aggregate_id = :id
+        WHERE stream = :id
         ORDER BY version
         """
         res = self.connection.execute(text(query), id=id_)
